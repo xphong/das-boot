498c199285749be5d7a8b17898d09ea30d85327a
Phong Huynh <phuynh@architech.ca>
2016-03-17 16:01:42 -0400

    Create shipwreck repository, hook controller to JPA

--- src/main/java/com/boot/controller/ShipwreckController.java
+++ src/main/java/com/boot/controller/ShipwreckController.java
@@ -1,6 +1,9 @@
 package com.boot.controller;
 
 import com.boot.model.Shipwreck;
+import com.boot.repository.ShipwreckRepository;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -9,28 +12,35 @@ import java.util.List;
 @RequestMapping("api/")
 public class ShipwreckController {
 
+    @Autowired
+    private ShipwreckRepository shipwreckRepository;
+
     @RequestMapping(value = "shipwrecks", method = RequestMethod.GET)
     public List<Shipwreck> list() {
-        return ShipwreckStub.list();
+        return shipwreckRepository.findAll();
     }
 
     @RequestMapping(value = "shipwrecks", method = RequestMethod.POST)
     public Shipwreck create(@RequestBody Shipwreck shipwreck) {
-        return ShipwreckStub.create(shipwreck);
+        return shipwreckRepository.saveAndFlush(shipwreck);
     }
 
     @RequestMapping(value = "shipwrecks/{id}", method = RequestMethod.GET)
     public Shipwreck get(@PathVariable Long id) {
-        return ShipwreckStub.get(id);
+        return shipwreckRepository.findOne(id);
     }
 
     @RequestMapping(value = "shipwrecks/{id}", method = RequestMethod.PUT)
     public Shipwreck update(@PathVariable Long id, @RequestBody Shipwreck shipwreck) {
-        return ShipwreckStub.update(id, shipwreck);
+        Shipwreck existingShipwreck = shipwreckRepository.findOne(id);
+        BeanUtils.copyProperties(shipwreck, existingShipwreck);
+        return shipwreckRepository.saveAndFlush(existingShipwreck);
     }
 
     @RequestMapping(value = "shipwrecks/{id}", method = RequestMethod.DELETE)
     public Shipwreck delete(@PathVariable Long id) {
-        return ShipwreckStub.delete(id);
+        Shipwreck existingShipwreck = shipwreckRepository.findOne(id);
+        shipwreckRepository.delete(existingShipwreck);
+        return existingShipwreck;
     }
 }
