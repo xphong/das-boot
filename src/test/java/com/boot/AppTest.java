f30ac2f359d61788cbb160100d7ad670637a07ee
Phong Huynh <phuynh@architech.ca>
2016-03-18 10:15:53 -0400

    Add unit tests

--- src/test/java/com/boot/AppTest.java
+++ src/test/java/com/boot/AppTest.java
@@ -1,38 +1,16 @@
 package com.boot;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import com.boot.controller.HomeController;
+import org.junit.Test;
 
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
-    }
+import static org.junit.Assert.assertEquals;
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
+public class AppTest {
 
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
+    @Test
+    public void testApp() {
+        HomeController hc = new HomeController();
+        String result = hc.home();
+        assertEquals(result, "Das boot");
     }
 }
---
+++ src/test/java/com/boot/ShipwreckControllerTest.java
@@ -0,0 +1,37 @@
+package com.boot;
+
+import com.boot.controller.ShipwreckController;
+import com.boot.model.Shipwreck;
+import com.boot.repository.ShipwreckRepository;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import static org.mockito.Mockito.*;
+import static org.junit.Assert.assertEquals;
+
+public class ShipwreckControllerTest {
+    @InjectMocks
+    private ShipwreckController sc;
+
+    @Mock
+    private ShipwreckRepository shipwreckRepository;
+
+    @Before
+    public void init() {
+        MockitoAnnotations.initMocks(this);
+    }
+
+    @Test
+    public void testShipwreckGet() {
+        Shipwreck sw = new Shipwreck();
+        sw.setId(1l);
+        when(shipwreckRepository.findOne(1l)).thenReturn(sw);
+
+        Shipwreck wreck = sc.get(1L);
+        verify(shipwreckRepository).findOne(1l);
+        assertEquals(1l, wreck.getId().longValue());
+    }
+}
